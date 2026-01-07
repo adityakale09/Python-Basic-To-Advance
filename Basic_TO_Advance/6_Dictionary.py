4cbfb3419ee235905be7f7654e64a691176ec62c
adityakale09 <kaleshrikrushna6@gmail.com>
2026-01-07 23:33:24 +0530

    new file added

---
+++ Basic_TO_Advance/6_Dictionary.py
@@ -0,0 +1,65 @@
+# =============================Dictionary==============================
+# Dictionaries are used to store data values in key:value pairs.
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+
+d = {1: "Aditya",
+      2: "Kale" ,
+      3: 500}
+print(d)
+
+print("---------------------------------")
+# Dictionary items are presented in key:value pairs, and can be referred to 
+# by using the key name.
+
+# Example
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+print("---------------------------------")
+
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+print(len(thisdict))
+print(type(thisdict))
+print(thisdict)
+print("---------------------------------")
+
+# =====================The dict() Constructor============================\
+
+# It is also possible to use the dict() constructor to make a dictionary.
+# Example
+# Using the dict() method to make a dictionary:
+
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
+print("---------------------------------")
+
+d = dict(name = 'aditya', lname = 'kale' , div = 'b_final year')
+print(len(d))
+print("---------------------------------")
+
+
+print("---------------------------------")
+
+print("---------------------------------")
+
+print("---------------------------------")
+
+print("---------------------------------")
+
+print("---------------------------------")
+
+print("---------------------------------")
+
+print("---------------------------------")
+
+print("---------------------------------")
+
+print("---------------------------------")
