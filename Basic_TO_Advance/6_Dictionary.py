bd7928d60134e31d2947766ee7f68d75a9f616f3
adityakale09 <kaleshrikrushna6@gmail.com>
2026-01-14 19:42:47 +0530

    code added successfully

--- Basic_TO_Advance/6_Dictionary.py
+++ Basic_TO_Advance/6_Dictionary.py
@@ -41,9 +41,137 @@ thisdict = dict(name = "John", age = 36, country = "Norway")
 print(thisdict)
 print("---------------------------------")
 
+
 d = dict(name = 'aditya', lname = 'kale' , div = 'b_final year')
 print(len(d))
 print(type(d))
+
+x = d["name"]
+print(x.upper())  # convert value of name to upper case 
+print("---------------------------------")
+
+y = d.get('div')
+print(y)
+print("---------------------------------")
+
+# The keys() method will return a list of all the keys in the dictionary.
+
+print(d.keys())
+print("---------------------------------")
+
+# Update the "year" of the car by using the update() method:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
+print("---------------------------------")
+
+#====================================================================
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+a = car.keys() # prints keys in dictonary
+print(a) 
+print("---------------------------------")
+
+b = car.values() # prints values in dictonary
+print(b)
+print("---------------------------------")
+
+c = car.items() # prints key-value pairs of dictonary
+print(c)
+print('----------------------------------')
+
+#===================================================================
+car["colour"] = 'white'  # adding key pair in dictonary
+print(a)
+print("---------------------------------")
+
+car["model"] = "fortuner" # change the value of a key
+print(car)
+print("---------------------------------")
+
+print ('brand' in car) # checks the key is in dictonry or not
+print("---------------------------------")
+
+# =======================Python - Remove Dictionary Items===============================
+
+#The pop() method removes the item with the specified key name:
+c = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+a = c.pop("model")
+print(a)
+print("---------------------------------")
+
+print(c.popitem())  # popitem() only display/remove last item from dictonary
+print("---------------------------------")
+
+# delete entire list 
+# d = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# del d
+# print(d)  # rise error
+print("---------------------------------")
+
+# The clear() method empties the dictionary:
+d = {
+    "name" : "aditya",
+    "lname": "kale",
+    "age" : 22
+}
+print(d)
+
+d.clear()
+print(d)
+print("---------------------------------")
+
+# ======================Loop Dictionaries=================================
+
+# Print all key names in the dictionary, one by one:
+d = {
+    "name" : "aditya",
+    "lname": "kale",
+    "age" : 22
+}
+for x in d:
+    print(x)
+print("---------------------------------")
+
+# Print all values in the dictionary, one by one:
+d = {
+    "name" : "aditya",
+    "lname": "kale",
+    "age" : 22
+}
+for x in d:
+    print(d[x])
+print("---------------------------------")
+
+# Print all values in the dictionary, one by one:
+for x in d.values() :
+    print(x)
+print("---------------------------------")
+
+# Loop through both keys and values, by using the items() method:
+for x,y in d.items() :
+    print(x,y)
+print("---------------------------------")
+
+# or 
+for x in d.items():
+    print(x)
 print("---------------------------------")
 
 
@@ -57,10 +185,3 @@ print("---------------------------------")
 
 print("---------------------------------")
 
-print("---------------------------------")
-
-print("---------------------------------")
-
-print("---------------------------------")
-
-print("---------------------------------")
